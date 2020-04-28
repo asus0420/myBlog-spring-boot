f4c9adc3974b8d73a04dbc17b5dbe699a2a1f131
asus0420 <45875318+asus0420@users.noreply.github.com>
2020-04-28 20:02:52 +0800

    2020年4月28日 20:02:48

---
+++ src/main/java/com/xwgames/blog/web/IndexController.java
@@ -0,0 +1,21 @@
+package com.xwgames.blog.web;
+
+import com.xwgames.blog.NotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id, @PathVariable String name){
+        //int i = 9/0;
+//        String blog = null;
+//        if (blog == null){
+//            throw new NotFoundException("博客不存在");
+//        }
+        System.out.println("---------------index---------------");
+        return "index";
+    }
+}
---
+++ src/test/java/com/xwgames/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xwgames.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
